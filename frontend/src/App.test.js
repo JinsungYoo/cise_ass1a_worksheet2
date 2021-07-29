f0335ba8229a26376cf558ff31636f5000b1565a
JS <preketchup@gmail.com>
2021-07-29 13:41:41 +1200

    Testing Details Changed

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -10,6 +10,12 @@ import React from 'react';
 
     describe('Addition', () => {
       it('knows that 2 and 2 make 4', () => {
-        expect(2 + 2).toBe(4);
+        expect(2 + 2).toBe(6);
+      });
+    });
+
+    describe('Multiple', () => {
+      it('knows that 2 and 3 make 6', () => {
+        expect(2 * 3).toBe(6);
       });
     });
