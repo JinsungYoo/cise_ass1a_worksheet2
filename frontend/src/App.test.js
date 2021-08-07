02a731517d7b659830b9c69ff3899ec0ab327d9e
JS <preketchup@gmail.com>
2021-08-07 21:10:33 +1200

    Fixing Bug

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -8,12 +8,6 @@ import React from 'react';
       ReactDOM.unmountComponentAtNode(div);
     });
 
-    describe('Addition', () => {
-      it('knows that 2 and 2 make 4', () => {
-        expect(2 + 2).toBe(6);
-      });
-    });
-
     describe('Multiple', () => {
       it('knows that 2 and 3 make 6', () => {
         expect(2 * 3).toBe(6);
